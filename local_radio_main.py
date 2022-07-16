26ff5361f575d2945aa407bc92da64c749baf250
Lyric Doshi <lyric@mit.edu>
2022-07-16 14:24:35 -0700

    Refactor to make station sources independent.

--- local_radio_main.py
+++ local_radio_main.py
@@ -2,12 +2,15 @@ import local_radio
 
 def main():
     stations_directory = "/home/lyric/Documents/local-radio/stations"
+    directory_stations = local_radio.create_directory_stations(stations_directory)
+
+    stations = directory_stations
     play_keys = ['a', 's', 'd']
     change_station_previous_keys = ['q', 'w', 'e']
     change_station_next_keys = ['z', 'x', 'c']
 
     radio = local_radio.Radio(
-        stations_directory=stations_directory,
+        stations=stations,
         play_keys=play_keys,
         change_station_previous_keys=change_station_previous_keys,
         change_station_next_keys=change_station_next_keys)
