809ca98d06918fa7ad87287ae44990845ab54a50
Lyric Doshi <lyric@mit.edu>
2022-07-16 22:30:21 -0700

    Implement a SpotifyStation.

--- local_radio.py
+++ local_radio.py
@@ -1,10 +1,11 @@
-from typing import List, Tuple
+from typing import Any, Dict, List, Tuple
 
 import abc 
 import bisect
 import getch
 import mutagen
 import os
+import spotipy
 import time
 import vlc
 
@@ -102,7 +103,85 @@ class DirectoryStation(Station):
 
     def stop(self) -> None:
         self._media_list_player.stop()
-        
+
+
+class SpotifyStation(Station):
+
+    def __init__(self, spotify_client: spotipy.client.Spotify, device_id: str, playlist: Dict[str, Any]):
+        self.name = playlist['name']
+        self._spotify_client = spotify_client
+        self._device_id = device_id
+        self._playlist_uri = playlist['uri']
+
+        track_durations_ms = []
+        tracks = spotify_client.playlist(playlist['id'], fields="tracks")
+        for track in tracks['tracks']['items']:
+            track_durations_ms.append(track['track']['duration_ms'])
+        self._track_seeker = TrackSeeker(track_durations_ms)
+
+    def play(self) -> None:
+        track_index, track_start_time_ms = self._track_seeker.seek()
+
+        try:
+            self._spotify_client.start_playback(
+                device_id=self._device_id,
+                context_uri=self._playlist_uri,
+                offset={"position": track_index},
+                position_ms=track_start_time_ms)
+        except spotipy.exceptions.SpotifyException as e:
+            if e.code == -1 and e.http_status == 403:
+                # This occurs if the command player state already
+                # matches the command request. One way this can happen
+                # if the command is sent twice.
+                pass
+            else:
+                raise e
+
+        self._spotify_client.repeat("context", device_id=self._device_id)
+
+    def is_playing(self) -> bool:
+        return self._spotify_client.currently_playing()['is_playing']
+
+    def stop(self) -> None:
+        try:
+            self._spotify_client.pause_playback(device_id=self._device_id)
+        except spotipy.exceptions.SpotifyException as e:
+            if e.code == -1 and e.http_status == 403:
+                # This occurs if the command player state already
+                # matches the command request. One way this can happen
+                # if the command is sent twice.
+                pass
+            else:
+                raise e
+
+
+def create_directory_stations(stations_directory: str) -> List[Station]:
+    stations = []
+    
+    vlc_instance = vlc.Instance()
+    media_list_player = vlc.MediaListPlayer()
+    for station_name in sorted(os.listdir(stations_directory)):
+        station_path = os.path.join(stations_directory, station_name)
+        stations.append(DirectoryStation(
+            content_directory=station_path,
+            vlc_instance=vlc_instance,
+            media_list_player=media_list_player))
+
+    return stations
+
+def create_spotify_stations(spotify_client: spotipy.client.Spotify, device_id: str, playlist_name_prefix_filter="radio") -> List[Station]:
+    stations = []
+    playlists = spotify_client.current_user_playlists()
+    for playlist in playlists['items']:
+        if playlist['name'].startswith(playlist_name_prefix_filter):
+            stations.append(SpotifyStation(
+                spotify_client=spotify_client,
+                device_id=device_id,
+                playlist=playlist))        
+    
+    return stations
+
+
 class Radio:
 
     def __init__(self, stations_directory: str, play_keys: List[str], change_station_next_keys: List[str], change_station_previous_keys: List[str]):
@@ -142,13 +221,17 @@ class Radio:
                     self._current_station().play()
             elif command in self._change_station_previous_keys:
                 is_playing = self._current_station().is_playing()
-                self._current_station().stop()
+                if is_playing:
+                    self._current_station().stop()
                 self._change_station_previous()
                 if is_playing:
                     self._current_station().play()   
             elif command in self._change_station_next_keys:
                 is_playing = self._current_station().is_playing()
-                self._current_station().stop()
+                if is_playing:
+                    self._current_station().stop()
                 self._change_station_next()
                 if is_playing:
-                    self._current_station().play()   
+                    self._current_station().play()
+
+
--- local_radio_main.py
+++ local_radio_main.py
@@ -1,7 +1,28 @@
 import local_radio
+import spotipy
+
+# Spotify access.
+_SPOTIPY_CLIENT_ID='5b235f6968d5493db1dbda0e5f306f1a'
+_SPOTIPY_CLIENT_SECRET='8773a9fa89684e768883bfa733ab7875'
+_SPOTIPY_REDIRECT_URI='http://localhost'
+_SPOTIFY_SCOPE = ["user-modify-playback-state", "user-read-playback-state", "user-read-currently-playing", "playlist-read-private", "playlist-read-collaborative", "app-remote-control", "streaming", "user-library-read"]
+
+# lemmings player.
+_DEVICE_ID = "a582ca56f0f370cf4eae00f3f1fccaad174b15bb"
+
 
 def main():
     stations_directory = "/home/lyric/Documents/local-radio/stations"
+    directory_stations = local_radio.create_directory_stations(stations_directory)
+
+    spotify_client = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyOAuth(
+        client_id=_SPOTIPY_CLIENT_ID,
+        client_secret=_SPOTIPY_CLIENT_SECRET,
+        redirect_uri=_SPOTIPY_REDIRECT_URI,
+        scope=_SPOTIFY_SCOPE))
+    spotify_stations = local_radio.create_spotify_stations(spotify_client, _DEVICE_ID)
+
+    stations = directory_stations + spotify_stations
     play_keys = ['a', 's', 'd']
     change_station_previous_keys = ['q', 'w', 'e']
     change_station_next_keys = ['z', 'x', 'c']
